cdaa309625635a358c650f4ae71f2aad35f5c2c6
Williams Toro Susanibar <82621773+williamsts0102@users.noreply.github.com>
2024-06-24 20:23:39 -0500

    cambios

---
+++ SP's/2. SP_SAB_ListarAlerta.sql
@@ -0,0 +1,34 @@
+-----------------------------------------------------------------------------
+-- PROCEDIMIENTO: SP_SAB_ListarAlerta
+-- DESCRIPCIÓN: Este procedimiento lista las alertas almacenadas en la tabla tblAlerta.
+-- ENTRADAS:
+--     Ninguna.
+-- RETORNA:
+--     Listado de alertas con las columnas especificadas.
+-- NOTAS:
+--     - Este procedimiento asume que las alertas existen en la tabla tblAlerta.
+-- USO:
+--     EXEC SP_SAB_ListarAlerta;
+-- DESARROLLO: WILLIAMS TORO SUSANIBAR
+-- FECHA: 16 DE JUNIO DEL 2024
+-----------------------------------------------------------------------------  
+CREATE PROCEDURE SP_SAB_ListarAlerta
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Selección de columnas específicas de la tabla tblAlerta
+    SELECT
+        strCodAlerta,
+        strDepartamento,
+        strProvincia,
+        strDistrito,
+        strDescripcion,
+        strLatitud,
+        strLongitud,
+        bitEstado
+    FROM
+        tblAlerta
+    WHERE
+        bitEliminado = 0;  -- Considerando que bitEliminado = 0 significa que la alerta no está eliminada
+END;
---
+++ SP's/3. SP_SAB_DTS_ObtenerAlertaPorCodigo.sql
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------------------
+-- PROCEDIMIENTO: SP_SAB_DetalleAlerta
+-- DESCRIPCIÓN: Este procedimiento obtiene una alerta de la tabla tblAlerta
+--              basada en el código de alerta proporcionado.
+-- ENTRADAS:
+--     @pstrCodAlerta: Código de alerta a buscar.
+--                     Tipo de dato: VARCHAR(20).
+-- RETORNA:
+--     strCodAlerta: Código de alerta.
+--     strDepartamento: Departamento de la alerta.
+--     strProvincia: Provincia de la alerta.
+--     strDistrito: Distrito de la alerta.
+--     strDescripcion: Descripción de la alerta.
+--     strLatitud: Latitud de la alerta.
+--     strLongitud: Longitud de la alerta.
+--     bitEstado: Estado de la alerta (1 = Activa, 0 = Inactiva).
+-- NOTAS:
+--     - Si no se encuentra el código de alerta, retorna NULL en los campos.
+-- USO:
+--     EXEC SP_SAB_DetalleAlerta @pstrCodAlerta = 'CODIGOALERTA00001';
+-- DESARROLLO: WILLIAMS TORO SUSANIBAR
+-- FECHA: 16 DE JUNIO DEL 2024
+-----------------------------------------------------------------------------  
+CREATE PROCEDURE SP_SAB_DetalleAlerta
+    @pstrCodAlerta VARCHAR(20)
+AS
+BEGIN
+    -- Retorno de la alerta si se encuentra el código
+    SELECT 
+        strCodAlerta,
+        strDepartamento,
+        strProvincia,
+        strDistrito,
+        strDescripcion,
+        strLatitud,
+        strLongitud,
+        bitEstado
+    FROM tblAlerta
+    WHERE strCodAlerta = @pstrCodAlerta;
+END;
